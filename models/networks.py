937e35bb0b97d211e266c7e22909b984de7b7e25
Du-Ceh <94659742+Du-Ceh@users.noreply.github.com>
2021-11-24 16:55:39 +0800

    Update networks.py

--- models/networks.py
+++ models/networks.py
@@ -3,8 +3,6 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.utils.data
 import torch
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
 
 class conv_block(nn.Module):
     """
@@ -77,7 +75,9 @@ class AttentionBlock(nn.Module):
         psi = self.psi(psi)
         out = x * psi
         return out
-# --------------------------------------------------------------------------------------
+
+
+# ===================================================================================
 class AttU_Net(nn.Module):
     """
     Attention Unet implementation
@@ -163,10 +163,124 @@ class AttU_Net(nn.Module):
 
         return out
 
-    
-class Segm_Net(nn.Module):
+
+# ===================================================================================
+
+#CBAM
+class ChannelAttention(nn.Module):
+    def __init__(self, in_planes, ratio=32):
+        super(ChannelAttention, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.max_pool = nn.AdaptiveMaxPool2d(1)
+        
+        self.fc1   = nn.Conv2d(in_planes, in_planes // ratio, 1, bias=False)
+        self.relu1 = nn.ReLU()
+        self.fc2   = nn.Conv2d(in_planes // ratio, in_planes, 1, bias=False)
+ 
+        self.sigmoid = nn.Sigmoid()
+ 
+    def forward(self, x):
+        avg_out = self.fc2(self.relu1(self.fc1(self.avg_pool(x))))
+        max_out = self.fc2(self.relu1(self.fc1(self.max_pool(x))))
+        out = avg_out + max_out
+        return self.sigmoid(out)
+ 
+
+class SpatialAttention(nn.Module):
+    def __init__(self, kernel_size=3):
+        super(SpatialAttention, self).__init__()
+ 
+        assert kernel_size in (3, 7), 'kernel size must be 3 or 7'
+        padding = 3 if kernel_size == 7 else 1
+ 
+        self.conv1 = nn.Conv2d(2, 1, kernel_size, padding=padding, bias=False)
+        self.sigmoid = nn.Sigmoid()
+ 
+    def forward(self, x):
+        avg_out = torch.mean(x, dim=1, keepdim=True)
+        max_out, _ = torch.max(x, dim=1, keepdim=True)
+        x = torch.cat([avg_out, max_out], dim=1)
+        x = self.conv1(x)
+        return self.sigmoid(x)
+
+class AttDilatedResnetBlock(nn.Module):
+
+    def __init__(self, dim, padding_type='reflect', norm_layer=nn.BatchNorm2d, use_dropout=False, use_bias=False):
+
+        super(AttDilatedResnetBlock, self).__init__()
+        self.conv_block1 = self.build_conv_block(dim, padding_type, norm_layer, use_dropout=use_dropout, use_bias=use_bias, num_dilation=1, num_padding=1)
+        self.conv_block2 = self.build_conv_block(dim, padding_type, norm_layer, use_dropout=use_dropout, use_bias=use_bias, num_dilation=2, num_padding=2)
+        self.conv_block3 = self.build_conv_block(dim, padding_type, norm_layer, use_dropout=use_dropout, use_bias=use_bias, num_dilation=3, num_padding=3)
+        self.conv_block4 = self.build_conv_block(dim, padding_type, norm_layer, use_dropout=use_dropout, use_bias=use_bias, num_dilation=4, num_padding=4)
+        
+        self.joint1 = nn.Sequential(
+            nn.Conv2d(dim*2, dim, kernel_size=1, padding=0, bias=use_bias),
+            nn.ReLU(),
+            nn.BatchNorm2d(dim)
+        )
+
+        self.joint2 = nn.Sequential(
+            nn.Conv2d(dim*2, dim, kernel_size=1, padding=0, bias=use_bias),
+            nn.ReLU(),
+            nn.BatchNorm2d(dim)
+        )
+
+        self.ca = ChannelAttention(dim)
+        self.sa = SpatialAttention()
+
+
+    def build_conv_block(self, dim, padding_type, norm_layer, num_dilation, num_padding, use_dropout, use_bias):
+        
+        conv_block = []
+        p = 0
+        if padding_type == 'reflect':
+            conv_block += [nn.ReflectionPad2d(num_padding)]
+        elif padding_type == 'replicate':
+            conv_block += [nn.ReplicationPad2d(num_padding)]
+        elif padding_type == 'zero':
+            p = num_padding
+        else:
+            raise NotImplementedError('padding [%s] is not implemented' % padding_type)
+
+        conv_block += [nn.Conv2d(dim, dim, kernel_size=3, padding=p, bias=use_bias, dilation=num_dilation), norm_layer(dim), nn.ReLU(True)]
+        if use_dropout:
+            conv_block += [nn.Dropout(0.5)]
+
+        p = 0
+        if padding_type == 'reflect':
+            conv_block += [nn.ReflectionPad2d(num_padding)]
+        elif padding_type == 'replicate':
+            conv_block += [nn.ReplicationPad2d(num_padding)]
+        elif padding_type == 'zero':
+            p = num_padding
+        else:
+            raise NotImplementedError('padding [%s] is not implemented' % padding_type)
+        conv_block += [nn.Conv2d(dim, dim, kernel_size=3, padding=p, bias=use_bias, dilation=num_dilation), norm_layer(dim)]
+
+        return nn.Sequential(*conv_block)
+
+    def forward(self, x):
+        child1 = self.conv_block1(x)
+        child2 = self.conv_block2(x)
+        child3 = self.conv_block3(x)
+        child4 = self.conv_block4(x)
+
+        node1 = self.joint1(torch.cat((child1, child2), dim=1))
+        node2 = self.joint1(torch.cat((child3, child4), dim=1))
+        node = self.joint2(torch.cat((node1, node2), dim=1))
+        midL = node
+
+        node = self.ca(node) * node
+        node = self.sa(node) * node
+
+
+        # out = x + node  # add skip connections
+        out = x + node + midL  # add skip connections
+        return out
+
+class GLSP(nn.Module):
     def __init__(self, img_ch=3, output_ch=1, block_num = 6, ngf = 32):
-        super(Segm_Net, self).__init__()
+        super(GLSP, self).__init__()
 
         import functools
         norm_layer = nn.BatchNorm2d
@@ -199,28 +313,22 @@ class Segm_Net(nn.Module):
         # ================================
 
         self.Up5 = up_conv(filters[4], filters[3])
-        # self.Att5 = Attention_block(F_g=filters[3], F_l=filters[3], F_int=filters[2])
         self.Att5 = SpatialAttention()
         self.Up_conv5 = conv_block(filters[4], filters[3])
 
         self.Up4 = up_conv(filters[3], filters[2])
         self.Att4 = SpatialAttention()
-        # self.Att4 = Attention_block(F_g=filters[2], F_l=filters[2], F_int=filters[1])
         self.Up_conv4 = conv_block(filters[3], filters[2])
 
         self.Up3 = up_conv(filters[2], filters[1])
         self.Att3 = SpatialAttention()
-        # self.Att3 = Attention_block(F_g=filters[1], F_l=filters[1], F_int=filters[0])
         self.Up_conv3 = conv_block(filters[2], filters[1])
 
         self.Up2 = up_conv(filters[1], filters[0])
         self.Att2 = SpatialAttention()
-        # self.Att2 = Attention_block(F_g=filters[0], F_l=filters[0], F_int=32)
         self.Up_conv2 = conv_block(filters[1], filters[0])
 
         self.Conv = nn.Conv2d(filters[0], output_ch, kernel_size=1, stride=1, padding=0)
-
-        #self.active = torch.nn.Sigmoid()
 
 
     def forward(self, x):
@@ -244,34 +352,28 @@ class Segm_Net(nn.Module):
 
 
         d5 = self.Up5(e5)
-        # x4 = self.Att5(g=d5, x=e4)
         x4 = self.Att5(e4)*e4
         d5 = torch.cat((x4, d5), dim=1)
         d5 = self.Up_conv5(d5)
 
         d4 = self.Up4(d5)
-        # x3 = self.Att4(g=d4, x=e3)
         x3 = self.Att4(e3)*e3
         d4 = torch.cat((x3, d4), dim=1)
         d4 = self.Up_conv4(d4)
 
 
         d3 = self.Up3(d4)
-        # x2 = self.Att3(g=d3, x=e2)
         x2 = self.Att3(e2)*e2
         d3 = torch.cat((x2, d3), dim=1)
         d3 = self.Up_conv3(d3)
 
 
         d2 = self.Up2(d3)
-        # x1 = self.Att2(g=d2, x=e1)
         x1 = self.Att2(e1)*e1
         d2 = torch.cat((x1, d2), dim=1)
         d2 = self.Up_conv2(d2)
 
 
         out = self.Conv(d2)
-
-      #  out = self.active(out)
 
         return out
