e6480256e52fa82953160530d704e5007752b750
Du-Ceh <94659742+Du-Ceh@users.noreply.github.com>
2021-11-25 16:28:20 +0800

    Update train.py

--- train.py
+++ train.py
@@ -1,26 +1,22 @@
 # coding=utf-8
 import torch
-import torch.nn.functional as F
 import torch.nn as nn
-import argparse
-import os.path as osp
 import os
 import time
 import warnings
-os.environ["CUDA_VISIBLE_DEVICES"] = "5"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 from visualization import load_checkpoint, save_checkpoint
 from dataReader import DGDataset, DGDataLoader
 from losses import VGGLoss, GicLoss, segm_unet_loss
 from config import parser
-import numpy as np
-from model.networks import AttU_Net, GLSP
-from flownet import FlowNet
+from models.networks import AttU_Net, GLSP
+from flownet import AFGAN, flow_warping
 torch.set_printoptions(profile="full")
 warnings.filterwarnings("ignore")
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-def train_segm(opt, train_loader, model):
+def trainSGM(opt, train_loader, model):
     print('----Traning of module {} started----'.format(opt.name))
     model.to(device)
     model.train()
@@ -60,7 +56,7 @@ def train_segm(opt, train_loader, model):
             save_checkpoint(model, os.path.join(opt.checkpoint_dir, opt.name, 'step_%06d.pth' % (step+1)))
 
 
-def train_gmm(opt, train_loader, model):
+def trainGAM(opt, train_loader, model):
     print('----Traning of module {} started----'.format(opt.name))
     model.to(device)
     model.train()
@@ -69,7 +65,7 @@ def train_gmm(opt, train_loader, model):
     criterionVGG = VGGLoss()
     gicloss = GicLoss(opt)
 
-    #used scheduler for training of gmm
+    #used scheduler for training of GAM
     optimizer = torch.optim.Adam(model.parameters(), lr=opt.lr, betas=(0.5, 0.999))
     lr_lambda = lambda step: 1.0 - max(0, step - opt.keep_step) / float(opt.decay_step + 1)
     scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer, lr_lambda = lr_lambda)
@@ -85,7 +81,7 @@ def train_gmm(opt, train_loader, model):
         im_cm = seg_shape[:,4,:,:].unsqueeze(1)
 
         sin_flow = model(c, im_cm)
-        sample_im_s, grid = flow_warp(c, sin_flow)
+        sample_im_s, grid = flow_warping(c, sin_flow)
 
         warped_cloth = im_cm*sample_im_s
         
@@ -111,7 +107,7 @@ def train_gmm(opt, train_loader, model):
             save_checkpoint(model, os.path.join(opt.checkpoint_dir, opt.name, 'step_%06d.pth' % (step+1)))
 
 
-def train_tom(opt, train_loader, model):
+def trainTSM(opt, train_loader, model):
     print('----Traning of module {} started----'.format(opt.name))
     model.to(device)
     model.train()
@@ -119,15 +115,15 @@ def train_tom(opt, train_loader, model):
     criterionL1 = nn.L1Loss()
     criterionVGG = VGGLoss()
 
-    gmm = FlowNet(inpur_An=3, input_Bn=1, ngf=64, use_bias=True)
-    load_checkpoint(gmm, os.path.join(opt.checkpoint_dir, 'GMM', 'step_200000.pth'))
-    gmm.to(device)
-    gmm.eval()
+    afgan = AFGAN(inpur_An=3, input_Bn=1, ngf=64, use_bias=True)
+    load_checkpoint(afgan, os.path.join(opt.checkpoint_dir, 'AFGAN', 'gam_final.pth'))
+    afgan.to(device)
+    afgan.eval()
 
     #scheduler is not used during training
-    optimizer_G = torch.optim.Adam(model.parameters(), lr=opt.lr, betas=(0.5, 0.999))
-    lr_lambda_G = lambda step: 1.0 - max(0, step - opt.keep_step) / float(opt.decay_step + 1)
-    scheduler_G = torch.optim.lr_scheduler.LambdaLR(optimizer_G, lr_lambda = lr_lambda_G)
+    optimizer = torch.optim.Adam(model.parameters(), lr=opt.lr, betas=(0.5, 0.999))
+    lr_lambda = lambda step: 1.0 - max(0, step - opt.keep_step) / float(opt.decay_step + 1)
+    scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer, lr_lambda = lr_lambda)
 
     for step in range(opt.previous_step, opt.keep_step + opt.decay_step):
         iter_start_time = time.time()
@@ -140,33 +136,29 @@ def train_tom(opt, train_loader, model):
         seg_shape = inputs['seg_shape'].to(device)
         part_arms = inputs['part_arms'].to(device)
 
-
-        with torch.no_grad():
-            sin_flow = gmm(c, im_cm)
-            sample_im_s, _ = flow_warp(c, sin_flow)
-            cw = im_cm*sample_im_s
+        sin_flow = afgan(c, im_cm)
+        sample_im_s, _ = flow_warping(c, sin_flow)
+        cw = im_cm*sample_im_s
         
         arm_par = seg_shape[:,11,:,:].unsqueeze(1) + seg_shape[:,13,:,:].unsqueeze(1)
         input_tom = torch.cat((seg_shape, im_ttp, cw, part_arms*arm_par), 1)
         img_tryon = model(input_tom)
 
         # ############### Backward Pass ####################
-        loss_l1 = criterionL1(img_tryon, im)
-        loss_vgg = criterionVGG(img_tryon, im)
-        loss_G = loss_l1 + loss_vgg
+        loss = criterionL1(img_tryon, im) + torch.mean(criterionVGG(img_tryon, im))
 
         # update discriminator weights
-        optimizer_G.zero_grad()
-        loss_G.backward()
-        optimizer_G.step()
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
 
         if (step+1) % opt.display_count == 0:
             t = time.time() - iter_start_time
-            print('step: %8d, time: %.3f, loss_G: %4f' % (step+1, t, loss_G.item()), flush=True)
+            print('step: %8d, time: %.3f, loss: %4f' % (step+1, t, loss.item()), flush=True)
 
         if (step+1) % opt.save_count == 0:
             save_checkpoint(model, os.path.join(opt.checkpoint_dir, opt.name, 'step_%06d.pth' % (step+1)))
-
+ 
 
 def main():
     # opt = get_opt()
@@ -176,32 +168,32 @@ def main():
 
     # create dataset
     train_dataset = DGDataset(opt)
-
     # create dataloader
     train_loader = DGDataLoader(opt, train_dataset)
 
     # create model & train & save the final checkpoint
     if opt.stage == 'SGM':
-        model = GLSP(5, 21, 0, 32)
+
+        model = GLSP(6, 21, 0, 32)
 
         if not os.path.exists(os.path.join(opt.checkpoint_dir, opt.name)):
             os.makedirs(os.path.join(opt.checkpoint_dir, opt.name))
 
         if not opt.checkpoint == '' and os.path.exists(opt.checkpoint):
             load_checkpoint(model, opt.checkpoint)
-        train_segm(opt, train_loader, model)
-        save_checkpoint(model, os.path.join(opt.checkpoint_dir, opt.name, 'seg_final.pth'))
+        trainSGM(opt, train_loader, model)
+        save_checkpoint(model, os.path.join(opt.checkpoint_dir, opt.name, 'sgm_final.pth'))
 
     elif opt.stage == 'GAM':
 
-        model = FlowNet(inpur_An=3, input_Bn=1, ngf=64, use_bias=True)
+        model = AFGAN(inpur_An=3, input_Bn=1, ngf=64, use_bias=True)
         if not os.path.exists(os.path.join(opt.checkpoint_dir, opt.name)):
             os.makedirs(os.path.join(opt.checkpoint_dir, opt.name))
             
         if not opt.checkpoint =='' and os.path.exists(opt.checkpoint):
             load_checkpoint(model, opt.checkpoint)
-        train_gmm(opt, train_loader, model)
-        save_checkpoint(model, os.path.join(opt.checkpoint_dir, opt.name, 'gmm_final.pth'))
+        trainGAM(opt, train_loader, model)
+        save_checkpoint(model, os.path.join(opt.checkpoint_dir, opt.name, 'gam_final.pth'))
 
     elif opt.stage == 'TSM':
         model = AttU_Net(30, 3)
@@ -210,8 +202,8 @@ def main():
 
         if not opt.checkpoint =='' and os.path.exists(opt.checkpoint):
             load_checkpoint(model, opt.checkpoint)
-        train_tom(opt, train_loader, model)
-        save_checkpoint(model, os.path.join(opt.checkpoint_dir, opt.name, 'tom_final.pth'))
+        trainTSM(opt, train_loader, model)
+        save_checkpoint(model, os.path.join(opt.checkpoint_dir, opt.name, 'tsm_final.pth'))
     else:
         raise NotImplementedError('Model [%s] is not implemented' % opt.stage)
 
